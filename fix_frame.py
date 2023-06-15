0b3c24f30d5aef345be3f897189039902c63cbfe
Yuta_Domen <yutamarodd0730@gmail.com>
2023-06-15 14:38:30 +0900

    二次配列における行数(フレーム数)の調整

--- fix_frame.py
+++ fix_frame.py
@@ -1,3 +1,5 @@
+#テンプレートをcity011として未入力をcity012とするDPMソースコード
+
 import numpy as np
 import random
 
@@ -16,21 +18,25 @@ for i in range(1, 101):
     file_path = template_file_prefix + str(i).zfill(3) + file_extension
     line_counter = 0  # 行カウント
     file_data = []
-    with open(file_path) as f:
+    with open(file_path) as f: # テキストファイルを開く
         for line in f:
             line_counter += 1
             if line_counter < 4:  # 最初の数値に関係ない行をスキップ
                 continue
             try:
-                row = list(map(float, line.strip().split()))
-                file_data.append(row)
+                row = list(map(float, line.strip().split())) # 一行ずつ書き込む
+                file_data.append(row) # file_dataに一時的に格納
             except ValueError:
                 continue
-    if file_data:
+    if file_data: # file_dataが有効な時にappendを用いてテキストファイルのデータをそのまま二次元配列として
         template_data.append(file_data)
-    template_framenum.append(line_counter - 3)
+
+    # 各単語のフレーム数を1次元配列として
+    # x=0,1番目のフレーム数‥x=99,100番目のフレーム数
+    template_framenum.append(line_counter - 3) 
 
 # テンプレートデータを3次元配列に変換
+# deps = 単語番号-1, rows = 行-1, column = 列-1
 template_data_3d = np.array(template_data)
 
 # 未入力データの読み込み
@@ -38,24 +44,28 @@ for i in range(1, 101):
     file_path = uninput_file_prefix + str(i).zfill(3) + file_extension
     line_counter = 0  # 行カウント
     file_data = []
-    with open(file_path) as f:
+    with open(file_path) as f: #テキストファイルを開く
         for line in f:
             line_counter += 1
             if line_counter < 4:  # 最初の数値に関係ない行をスキップ
                 continue
             try:
-                row = list(map(float, line.strip().split()))
-                file_data.append(row)
+                row = list(map(float, line.strip().split())) # 一行ずつ書き込む
+                file_data.append(row) # file_dataに一時的に格納
             except ValueError:
                 continue
-    if file_data:
+    if file_data: # file_dataが有効な時にappendを用いてテキストファイルのデータをそのまま二次元配列として
         uninput_data.append(file_data)
+
+    # 各単語のフレーム数を1次元配列として
+    # x=0,1番目のフレーム数‥x=99,100番目のフレーム数
     uninput_framenum.append(line_counter - 3)
 
 # 未入力データを3次元配列に変換
+# deps = 単語番号-1, rows = 行-1, column = 列-1
 uninput_data_3d = np.array(uninput_data)
 
-
+# フレーム数を調整する
 for p in range(100):
     for q in range(100):
         template_data_3d_frame = np.array(template_data_3d[p])
@@ -63,7 +73,10 @@ for p in range(100):
         template_framenum_frame = template_framenum[p]
         uninput_framenum_frame = uninput_framenum[q]
 
+        # フレーム数の差をdiffとする
         diff = abs(template_framenum_frame - uninput_framenum_frame)
+
+        # テンプレ―トと未入力のフレームの大小を比較し、フレーム数が大きいデータの数の差分をランダムで行を削除する
 
         if template_framenum_frame < uninput_framenum_frame:
             random_numbers = random.sample(range(uninput_framenum_frame), diff)
@@ -76,7 +89,7 @@ for p in range(100):
             for d in range(diff):
                 template_framenum_frame -= 1
 
-    #確認表示
+    #shapeを用いてフレーム数の調整後の確認表示
     print("------------------")
     print("単語", p, "単語", q)
     print("テンプレートのフレーム数", uninput_framenum_frame)
